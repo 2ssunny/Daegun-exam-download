ad83e8968141ed8fc32ba8595d85f25c78bf69ae
Juho Lee <eungseoni@gmail.com>
2023-04-29 03:27:53 +0900

    style: add info

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,14 +3,30 @@ import { useState } from "react";
 import React from "react";
 
 import "./home.css";
+import githubimg from "../media/github.png";
 
 function Home() {
   return (
-    <div className="home">
-      <h1 className="home_title">Exam information download site</h1>
+    <div>
+      <div className="homehead">
+        <h1 className="home_title">
+          대건고등학교 기출문제 및 모의고사 다운로드 사이트
+        </h1>
 
-      <div className="homeexplain">
-        <p>webpage for Daegun hish school students</p>
+        <div className="homeexplain">
+          <p>대건고등학교 학생들의 편의를 위한 사이트입니다.</p>
+          <p>학생들의 편의를 위한것이며 영리적 목적으로 사용되지 않습니다.</p>
+        </div>
+      </div>
+      <div className="homefooter">
+        <p className="homename">
+          Made by <span>Lee Juho</span> of 2-7
+        </p>
+        <p className="homecontact">
+          <a href="https://github.com/2ssunny" target="_blank">
+            <img src={githubimg} className="homegithub" />
+          </a>
+        </p>
       </div>
     </div>
   );
