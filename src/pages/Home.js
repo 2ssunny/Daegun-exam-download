37a8ed58df7d2b658cdaa0ce5d2ba27f3ac43151
Juho Lee <eungseoni@gmail.com>
2023-05-15 09:44:24 +0900

    feat: add advertisement

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import "./home.css";
 import githubimg from "../media/github.png";
-
 function Home() {
   return (
     <div>
@@ -10,9 +9,19 @@ function Home() {
         <h1 className="home_title">대건고등학교 기출문제 다운로드 사이트</h1>
 
         <div className="homeexplain">
-          <p>Test</p>
           <p>대건고등학교 학생들의 편의를 위한 사이트입니다.</p>
           <p>학생들의 편의를 위한것이며 영리적 목적으로 사용되지 않습니다.</p>
+        </div>
+        <br />
+        <h2 className="ad_title">기타 학교 정보 제공 사이트</h2>
+        <div className="ldw_ad">
+          <a href="https://ii-dw.github.io/SchoolInformation/">
+            <p>대건고등학교 2학년 7반 정보확인사이트</p>
+          </a>
+
+          <a href="https://ii-dw.github.io/Math-Answer/">
+            <p>대건고등학교 수학2 형성평가 답지 제공 사이트</p>
+          </a>
         </div>
       </div>
       <div className="homefooter">
